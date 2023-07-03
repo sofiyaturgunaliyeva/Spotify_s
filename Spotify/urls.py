33ff69505a89bf215bda140123a4dabc89abae9e
sofiyaturgunaliyeva <sofiya2005t@gmail.com>
2023-07-03 10:42:06 +0500

    Albom va Qoshiq uchun ModelViewset yozildi

--- Spotify/urls.py
+++ Spotify/urls.py
@@ -1,13 +1,18 @@
-
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from app1.views import *
+from rest_framework.routers import DefaultRouter
+router = DefaultRouter()
+router.register("albomlar",AlbomModelViewset)
+router.register("qoshiqlar",QoshiqModelViewset)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('qoshiqchilar/', QoshiqchilarAPIView.as_view()),
     path('qoshiqchilar/', QoshiqchilarAPIView.as_view()),
-    path('qoshiqlar/', QoshiqlarAPIView.as_view()),
+    # path('qoshiqlar/', QoshiqlarAPIView.as_view()),
+    path('', include(router.urls)),
+    # path('albomlar/', AlbomlarAPIView.as_view()),
     path('bitta_qoshiqchi/<int:pk>/', QoshiqchiDetailView.as_view()),
-    path('bitta_qoshiq/<int:pk>/', QoshiqAPIView.as_view()),
+    # path('bitta_qoshiq/<int:pk>/', QoshiqAPIView.as_view()),
 ]
--- app1/views.py
+++ app1/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
+from rest_framework.decorators import action
 
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import *
 from .serializers import *
 from rest_framework import  status
+
+from rest_framework.viewsets import ModelViewSet
 
 
 # Vazifa
@@ -47,23 +50,65 @@ class QoshiqchiDetailView(APIView):
 
 
 
-class QoshiqlarAPIView(APIView):
-    def get(self, requset):
-        qoshiqlar = Qoshiq.objects.all()
+# class QoshiqlarAPIView(APIView):
+#     def get(self, requset):
+#         qoshiqlar = Qoshiq.objects.all()
+#         serializer = QoshiqSerializer(qoshiqlar, many=True)
+#         return Response(serializer.data)
+
+# class QoshiqAPIView(APIView):
+#     def get(self, request, pk):
+#         qoshiq = Qoshiq.objects.get(id=pk)
+#         serializer = QoshiqSerializer(qoshiq)
+#         return Response(serializer.data)
+#
+#     def put(self, request, pk):
+#         qoshiq = Qoshiq.objects.get(id=pk)
+#         malumot = request.data
+#         serializer = QoshiqSerializer(qoshiq, data=malumot)
+#         if serializer.is_valid():
+#             serializer.save()
+#             return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+# class AlbomlarAPIView(APIView):
+#     def get(self, requset):
+#         albomlar = Albom.objects.all()
+#         serializer = AlbomSerializer(albomlar, many=True)
+#         return Response(serializer.data)
+
+
+
+# Vazifa
+
+# 1. Albomlar uchun ModelViewSet yozing
+
+# 2. Bitta albomdan turib unga tegishli qo’shiq qo’shuvchi action qo’shing.
+
+# 3. Bitta albomdan turib unga tegishli qo’shiqlarni chiqaruvchi action qo’shing
+class AlbomModelViewset(ModelViewSet):
+    queryset = Albom.objects.all()
+    serializer_class = AlbomSerializer
+
+    @action(detail=True, methods=['Get','POST'])
+    def qoshiqlar(self, request, pk): # albomlar/1/qoshiqlar
+        albom = self.get_object()  # == Albom.objects.get(id = pk)
+        if request.method == 'POST':
+            serializer = QoshiqSerializer(data=request.data)
+            if serializer.is_valid():
+                serializer.save(albom=albom)
+                return Response(serializer.data, status=status.HTTP_201_CREATED)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        qoshiqlar = Qoshiq.objects.filter(albom=albom)
         serializer = QoshiqSerializer(qoshiqlar, many=True)
         return Response(serializer.data)
 
-class QoshiqAPIView(APIView):
-    def get(self, request, pk):
-        qoshiq = Qoshiq.objects.get(id=pk)
-        serializer = QoshiqSerializer(qoshiq)
-        return Response(serializer.data)
 
-    def put(self, request, pk):
-        qoshiq = Qoshiq.objects.get(id=pk)
-        malumot = request.data
-        serializer = QoshiqSerializer(qoshiq, data=malumot)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# 4. Qo’shiq jadvalini ham ModelViewSetda qayta yozing
+
+
+
+class QoshiqModelViewset(ModelViewSet):
+    queryset = Qoshiq.objects.all()
+    serializer_class = QoshiqSerializer
